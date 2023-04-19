fb411bc596fdc36078959f490d529d8bce0fe3b7
aronkahehe <113589448+aronkahehe@users.noreply.github.com>
2023-04-19 19:31:28 +0200

    Update placed_orders.php

--- admin/placed_orders.php
+++ admin/placed_orders.php
@@ -10,14 +10,7 @@ if(!isset($admin_id)){
    header('location:admin_login.php');
 }
 
-if(isset($_POST['update_payment'])){
-   $order_id = $_POST['order_id'];
-   $payment_status = $_POST['payment_status'];
-   $payment_status = filter_var($payment_status, FILTER_SANITIZE_STRING);
-   $update_payment = $conn->prepare("UPDATE `orders` SET payment_status = ? WHERE id = ?");
-   $update_payment->execute([$payment_status, $order_id]);
-   $message[] = 'Fizetési mód frissítve!';
-}
+
 
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
